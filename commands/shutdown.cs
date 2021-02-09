b513cf7115344ff63255b8051a7b6726657c5c87
Tarith <tarithj@gmail.com>
2021-02-09 23:01:23 +0530

    created shutdown command

---
+++ commands/shutdown.cs
@@ -0,0 +1,20 @@
+﻿using Cosmos.System.FileSystem;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sphere_os.commands
+{
+    class Shutdown: command
+    {
+        public override void getInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Run(CommandInput ci)
+        {
+            Cosmos.System.Power.Shutdown();
+        }
+    }
+}
