0899569bdbb3e3f06ee47e738a143ba338e67a24
Tarith <tarithj@gmail.com>
2021-02-09 20:50:40 +0530

    Created file system utilities

---
+++ filesystem/fs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sphere_os.filesystem
+{
+   static class Utils
+    {
+        public static string[] PathToArray(string p)
+        {
+            return p.Split("/");
+        }
+
+        // returns the path before the current path
+        // example
+        // string d = "a/b/c/d"
+        // string b = fsTrack.GoBackADir(d)
+        // Console.WriteLine(d) $ a/b/c
+        public static string GoBackADir(string dir)
+        {
+            string[] a = PathToArray(dir);
+            if (a.Length == 1)
+            {
+                return "/";
+            }
+            dir = a[a.Length - 1];
+            return dir;
+        }
+    }
+
+}
