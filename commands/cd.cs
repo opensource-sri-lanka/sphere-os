23b16451fb59bdab339aeb9e2aeff3006db81d03
Tarith <tarithj@gmail.com>
2021-02-09 21:39:17 +0530

    created cd command

---
+++ commands/cd.cs
@@ -0,0 +1,32 @@
+﻿using Cosmos.System.FileSystem;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sphere_os.commands
+{
+    class CD: command
+    {
+        public override void getInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Run(CosmosVFS vfs, filesystem.Dir dir, string[] input)
+        {
+            try
+            {
+                var fDir = input[1];
+                var d = vfs.GetDirectory(fDir);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("cant find directory: " + e.Message);
+            }
+            finally
+            {
+                dir.dir = dir + "/" + input[1];
+            }
+        }
+    }
+}
