b104cd2cb6c0aed64be3f933634f187bace35a65
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-09 19:53:27 +0530

    add license to .cs file

--- Kernel.cs
+++ Kernel.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿// main file
+/* Copyright (C) 2021 Dinuda Yaggahavita, Tarith Jayasooriya
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Sys = Cosmos.System;
@@ -9,7 +23,8 @@ namespace sphere_os
     {
         protected override void BeforeRun()
         {
-            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+            Console.WriteLine("< program > Copyright(C) < year >  < name of author >");
+            Console.WriteLine("This program comes with ABSOLUTELY NO WARRANTY;  This is free software, and you are welcome to redistribute it");
         }
 
         protected override void Run()
