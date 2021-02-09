3805e7836df039ea1fd139811e0556aaa696dc7e
Tarith <tarithj@gmail.com>
2021-02-09 23:00:34 +0530

    changed command.Run signature

--- commands/command.cs
+++ commands/command.cs
@@ -3,5 +3,13 @@ using sphere_os.filesystem;
 public abstract class command
 {
     public abstract void getInfo();
-    public abstract void Run(Cosmos.System.FileSystem.CosmosVFS vfs, Dir dir, string[] input);
+    public abstract void Run(CommandInput ci);
+
+}
+
+public class CommandInput
+{
+    public Cosmos.System.FileSystem.CosmosVFS vfs;
+    public Dir dir;
+    public string[] input;
 }
